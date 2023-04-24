10f22269b81f6085b49850ebc89f9428d96fb03f
shaharzelig <shahar@zelig.me>
2023-04-24 19:19:47 +0300

    Adding first template of flask application.

---
+++ app.py
@@ -0,0 +1,18 @@
+#!flask/bin/python
+from flask import Flask, jsonify, abort, make_response, request
+
+app = Flask(__name__)
+@app.route('/entry', methods=['POST'])
+def get_tasks():
+    plate = request.args.get('plate')
+    parking_lot = request.args.get('parkingLot')
+
+    return jsonify({'plate': plate, 'parkingLot': parking_lot})
+@app.route('/exit', methods=['POST'])
+def exit(task_id):
+    ticket_id = request.args.get('ticketId')
+    return jsonify({'ticketId': ticket_id})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
