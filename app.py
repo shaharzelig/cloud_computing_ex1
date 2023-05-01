e90c0cb69817c534d79e3388bb40144f6c6dba6f
shaharzelig <shahar@zelig.me>
2023-05-02 00:00:03 +0300

    Adding logic to app.py

--- app.py
+++ app.py
@@ -1,17 +1,35 @@
 #!flask/bin/python
-from flask import Flask, jsonify, abort, make_response, request
+import time
 
+from flask import Flask, jsonify, abort, make_response, request
+import random
 app = Flask(__name__)
+
+# No data persistence for now
+DB = {}
+
 @app.route('/entry', methods=['POST'])
-def get_tasks():
+def entry():
     plate = request.args.get('plate')
     parking_lot = request.args.get('parkingLot')
+    ticket_id = str(random.randint(0, 10000)).zfill(5)
+    while ticket_id in DB.keys():
+        ticket_id = str(random.randint(0, 10000)).zfill(5)
 
-    return jsonify({'plate': plate, 'parkingLot': parking_lot})
+    DB[ticket_id] = {'plate': plate, 'parkingLot': parking_lot, "time": int(time.time())}
+    return ticket_id
+
 @app.route('/exit', methods=['POST'])
-def exit(task_id):
+def exit():
     ticket_id = request.args.get('ticketId')
-    return jsonify({'ticketId': ticket_id})
+    if ticket_id not in DB.keys():
+        abort(404)
+
+    entry = DB[ticket_id]
+    total_time_in_minutes = (int(time.time()) - entry['time']) / 60
+    total_amount_of_15_minutes = total_time_in_minutes / 15
+    charge = total_amount_of_15_minutes * 2.5 # 2.5 USD for 15 minutes == 10 USD per hour
+    return jsonify({"parkingLot": entry['parkingLot'], "plate": entry['plate'], "charge": charge})
 
 
 if __name__ == '__main__':
