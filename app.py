09bb27d5a7d83b4d96d64ca7eedd88cf2bbebbbc
shaharzelig <shahar@zelig.me>
2023-05-02 01:11:06 +0300

    Fine tuning - app.py

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 #!flask/bin/python
 import time
 
-from flask import Flask, jsonify, abort, make_response, request
+from flask import Flask, jsonify, abort, request
 import random
 app = Flask(__name__)
 
-# No data persistence for now
+# No data persistence for now :)
 DB = {}
+
 
 @app.route('/entry', methods=['POST'])
 def entry():
     plate = request.args.get('plate')
     parking_lot = request.args.get('parkingLot')
+    if not plate or not parking_lot:
+        abort(400, "Missing plate or parkingLot parameter")
+
     ticket_id = str(random.randint(0, 10000)).zfill(5)
     while ticket_id in DB.keys():
         ticket_id = str(random.randint(0, 10000)).zfill(5)
@@ -19,16 +23,17 @@ def entry():
     DB[ticket_id] = {'plate': plate, 'parkingLot': parking_lot, "time": int(time.time())}
     return ticket_id
 
+
 @app.route('/exit', methods=['POST'])
-def exit():
+def parking_exit():
     ticket_id = request.args.get('ticketId')
     if ticket_id not in DB.keys():
-        abort(404)
+        abort(400, "Invalid ticketId parameter")
 
-    entry = DB[ticket_id]
-    total_time_in_minutes = (int(time.time()) - entry['time']) / 60
+    db_entry = DB[ticket_id]
+    total_time_in_minutes = (int(time.time()) - db_entry['time']) / 60
     total_amount_of_15_minutes = total_time_in_minutes / 15
-    charge = total_amount_of_15_minutes * 2.5 # 2.5 USD for 15 minutes == 10 USD per hour
+    charge = total_amount_of_15_minutes * 2.5   # 2.5 USD for 15 minutes == 10 USD per hour
     return jsonify({"parkingLot": entry['parkingLot'], "plate": entry['plate'], "charge": charge})
 
 
